39ae12c8f8d802b5b932e47cff52eba7c93520d2
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-10-06 17:15:05 -0400

    Get genre and year. Remove track from playlist when posting.

--- reddit.js
+++ reddit.js
@@ -22,6 +22,29 @@ const spotify = new spotifyWebApi({
 let tracks = [];
 spotify.setRefreshToken(process.env.S_REFRESH_TOKEN);
 
+async function getGenre(artist) {
+  try {
+    if (artist != undefined) {
+      // Assuming the first artist represents the main artist for the track
+      const artistId = artist.id;
+      const artistInfo = await spotify.getArtist(artistId);
+
+      if (artistInfo.body.genres.length > 0) {
+        const genre = artistInfo.body.genres[0];
+        return artistInfo.body.genres[0];
+      } else {
+        return 'indie';
+      }
+    } else {
+      console.warn('No artist provided.');
+      return null;
+    }
+  } catch (error) {
+    console.error('Error:', error);
+    return null;
+  }
+}
+
 async function refreshTracksList() {
   await spotify.refreshAccessToken().then(
     function (data) {
@@ -34,11 +57,14 @@ async function refreshTracksList() {
   );
 
   return spotify.getPlaylistTracks(process.env.S_PLAYLIST).then(
-    function (data) {
+    async function (data) {
       let temp = [];
       for (let i = 0; i < data.body.items.length; i++) {
-        console.log('Adding', data.body.items[i].track.artists[0].name, '-', data.body.items[i].track.name);
-        temp.push(`${data.body.items[i].track.artists[0].name} - ${data.body.items[i].track.name}`);
+        let track = data.body.items[i].track;
+        console.log('Adding', track.artists[0].name, '-', track.name);
+        
+        let genre = await getGenre(track.artists[0])
+        temp.push(`${track.artists[0].name} - ${track.name} [${genre}] (${track.album.release_date.substring(0,4)}):${track.id}`);
       }
       return temp;
     },
@@ -48,29 +74,36 @@ async function refreshTracksList() {
   );
 }
 
+async function removeTrackFromPlaylist(playlistId, trackId) {
+  try {
+    const response = await spotify.removeTracksFromPlaylist(playlistId, [{ uri: `spotify:track:${trackId}` }]);
+    
+    if (response.body.snapshot_id) {
+      console.log('Track removed successfully');
+    } else {
+      console.error('Error removing track:', response.body);
+    }
+  } catch (error) {
+    console.error('Error removing track:', error);
+  }
+}
+
 async function searchYoutube(query) {
   const apiKey = process.env.Y_API_KEY;
   const apiUrl = "https://www.googleapis.com/youtube/v3/search";
-  
-  const maxResults = 1; // Adjust as needed
-  
+
+  const maxResults = 1;
+
   // Construct the API request URL
   const requestUrl = `${apiUrl}?part=snippet&q=${encodeURIComponent(query)}&maxResults=${maxResults}&key=${apiKey}`;
-  
+
   try {
     const response = await fetch(requestUrl);
     const data = await response.json();
-    
+
     if (response.ok) {
       const videos = data.items;
-      
-      // Process and display video information
-      videos.forEach(video => {
-        console.log(`Title: ${video.snippet.title}`);
-        console.log(`Video ID: ${video.id.videoId}`);
-        console.log("---");
-      });
-      
+
       return `https://www.youtube.com/watch?v=${videos[0].id.videoId}`;
     } else {
       console.error(`Error: ${response.statusText}`);
@@ -90,20 +123,24 @@ function postRedditLink(title, link, subreddit) {
 }
 
 let postInterval = setInterval(async () => {
-  
+
   if (tracks.length <= 0) {
     tracks = await refreshTracksList();
   }
-  
+
   if (tracks.length <= 0) {
-    console.warn('Track list is empty!')
+    console.log("---");
+    console.log('---', '\n', `[${new Date(Date.now()).toLocaleString()}] No new tracks to post!`, '\n', '---')
     return;
   }
 
-  let url = await searchYoutube(tracks[tracks.length - 1]);
-  postRedditLink('tracks[0]', url, 'test_automation');
-  console.log('Posted', tracks[tracks.length - 1], `from ${url}`);
+  // let url = await searchYoutube(tracks[tracks.length - 1]);
+  let url = 'url';
+  let trackAndId = tracks[tracks.length - 1].split(':');
+  // postRedditLink(trackAndId[0], url, 'test_automation');
+  console.log(`[${new Date(Date.now()).toLocaleString()}]`, 'Posted', trackAndId[0], `from ${url}`);
 
+  removeTrackFromPlaylist(process.env.S_PLAYLIST, trackAndId[1]);
   tracks.pop();
 }, 3000);
 
