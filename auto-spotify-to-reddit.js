6967d7b164bbbb8dda21b03de5b858ca196d4986
Justin Linton <127802772+aberrator9@users.noreply.github.com>
2023-10-28 20:21:58 -0400

    Add logger module.

--- auto-spotify-to-reddit.js
+++ auto-spotify-to-reddit.js
@@ -8,41 +8,43 @@ const he = require('he');
 const stringSimilarity = require("string-similarity");
 const { time } = require('console');
 
+const Logger = require('./modules/logger.js');
+
 const repo = 'https://github.com/aberrator9/automation';
 const localEnv = dotenv.config();
 
-function checkForEnvironmentVariables() {
-  if (!fs.existsSync('.env')) {
-    const envContent = `R_CLIENT_ID=''\nR_CLIENT_SECRET=''\nR_REFRESH_TOKEN=''\nR_ACCESS_TOKEN=''\nR_USER=''\nR_PASS=''\nS_CLIENT_ID=''\nS_CLIENT_SECRET=''\nS_ACCESS_TOKEN=''\nS_REFRESH_TOKEN=''\nS_PLAYLIST=''\nY_API_KEY=''`;
-    fs.writeFileSync('.env', envContent);
-    console.log(`.env file created in local directory, but you will need to finish setting it up! See ${repo} for setup instructions.`)
-    console.log('Exiting...');
-    process.exit();
-  } else {
-    const missingValues = Object.entries(localEnv.parsed).filter(([key, value]) => value === '');
-    if (missingValues.length > 0) {
-      console.log(`.env file is missing required values! See ${repo} for setup instructions.`);
-      console.log('Exiting...');
-      process.exit();
-    }
-    else {
-      console.log('.env file found in local directory.');
-    }
-  }
-}
-
 function checkForFile(filename) {
-  if(!fs.existsSync(filename)) {
+  if (!fs.existsSync(filename)) {
     fs.writeFileSync(filename, filename.includes('json') ? '[]' : '');
   }
   return filename;
 }
 
-checkForEnvironmentVariables();
-const postedJson = checkForFile('posted.json');
-const notPostedJson = checkForFile('notPosted.json');
 const logFile = checkForFile('logs/beetbot.log');
 const sessionLogFile = checkForFile(`logs/beetbot.${Date.now()}.log`);
+const logger = new Logger(logFile, sessionLogFile);
+
+function checkForEnvironmentVariables() {
+  if (!fs.existsSync('.env')) {
+    const envContent = `R_CLIENT_ID=''\nR_CLIENT_SECRET=''\nR_REFRESH_TOKEN=''\nR_ACCESS_TOKEN=''\nR_USER=''\nR_PASS=''\nS_CLIENT_ID=''\nS_CLIENT_SECRET=''\nS_ACCESS_TOKEN=''\nS_REFRESH_TOKEN=''\nS_PLAYLIST=''\nY_API_KEY=''`;
+    fs.writeFileSync('.env', envContent);
+    logger.log(`.env file created in local directory, but you will need to finish setting it up! See ${repo} for setup instructions.`)
+    logger.log('Exiting...');
+    process.exit();
+  } else {
+    const missingValues = Object.entries(localEnv.parsed).filter(([key, value]) => value === '');
+    if (missingValues.length > 0) {
+      logger.log(`.env file is missing required values! See ${repo} for setup instructions.`);
+      logger.log('Exiting...');
+      process.exit();
+    }
+  }
+}
+
+checkForEnvironmentVariables();
+
+const postedJson = checkForFile('posted.json');
+const notPostedJson = checkForFile('notPosted.json');
 
 const reddit = new snoowrap({
   userAgent: 'anything',
@@ -69,7 +71,7 @@ function loadPostedTracks() {
     const data = fs.readFileSync(postedJson, 'utf8');
     return JSON.parse(data) || [];
   } catch (error) {
-    console.log('Error loading posted tracks:', error);
+    logger.log(`Error loading posted tracks: ${error}`);
     return [];
   }
 }
@@ -78,7 +80,7 @@ function savePostedTracks() {
   try {
     fs.writeFileSync(postedJson, JSON.stringify(postedTracks), 'utf8');
   } catch (error) {
-    console.log('Error saving posted tracks:', error);
+    logger.log(`Error saving posted tracks: ${error}`);
   }
 }
 
@@ -97,8 +99,8 @@ async function getGenre(artist) {
     } else {
       return 'indie'; // Anything that doesn't have a genre is 'indie'
     }
-  } catch (error) {
-    console.log('Error:', error);
+  } catch (err) {
+    logger.log(`Error getting genre: ${err}`);
   }
 }
 
@@ -108,7 +110,7 @@ async function refreshTracksList() {
       spotify.setAccessToken(data.body['access_token']);
     },
     function (err) {
-      console.log('Could not refresh access token', err);
+      logger.log(`Could not refresh access token: ${err}`);
     }
   );
 
@@ -121,11 +123,11 @@ async function refreshTracksList() {
         const id = track.id;
         const song = track.name;
 
-        if (cachedTracks.includes(id) || postedTracks.includes(id)) {
-          console.log('Track', `${artist} - ${song}`, 'already posted; removing from playlist.')
+        if (cachedTracks.includes(id) || postedTracks.includes(id) || notpos) {
+          logger.log(`Track ${artist} - ${song} already posted; removing from playlist.`);
           await removeTrackFromPlaylist(process.env.S_PLAYLIST, id);
         } else {
-          console.log('Adding', artist, '-', song);
+          logger.log(`Adding ${artist} - ${song}`);
 
           const genre = await getGenre(track.artists[0])
           temp.push({ postTitle: `${artist} - ${song} [${genre}] (${track.album.release_date.substring(0, 4)})`, id: id });
@@ -134,7 +136,7 @@ async function refreshTracksList() {
       return temp;
     },
     function (err) {
-      console.log(err);
+      logger.log(err);
     }
   );
 }
@@ -142,8 +144,8 @@ async function refreshTracksList() {
 async function removeTrackFromPlaylist(playlistId, trackId) {
   try {
     await spotify.removeTracksFromPlaylist(playlistId, [{ uri: `spotify:track:${trackId}` }]);
-  } catch (error) {
-    console.log('Error removing track:', error);
+  } catch (err) {
+    logger.log(`Error removing track: ${err}`);
   }
 }
 
@@ -163,22 +165,22 @@ async function searchYoutube(query) {
 
       const titleWithoutEntities = he.decode(videos[0].snippet.title);
       let similarity = stringSimilarity.compareTwoStrings(titleWithoutEntities.toLowerCase(), query.toLowerCase());
-      console.log(`Youtube returned ${titleWithoutEntities}, has a similarity of ${similarity} to ${query}`);
+      logger.log(`Youtube returned ${titleWithoutEntities}, has a similarity of ${similarity} to ${query}`);
 
       if (similarity <= STRING_SIMILARITY_THRESHOLD) {
         // Retry comparison with channel name
         const withChannelTitle = videos[0].snippet.channelTitle.split('-')[0] + '- ' + videos[0].snippet.title;
         similarity = stringSimilarity.compareTwoStrings(withChannelTitle.toLowerCase(), query.toLowerCase());
         // Check with channel title, minus '- Topic' for autogenerated music channels
-        console.log('With channel title, new similarity score is ', similarity, withChannelTitle);
+      logger.log(`With channel title, new similarity score is ${similarity}: ${withChannelTitle}`);
       }
 
       return similarity > STRING_SIMILARITY_THRESHOLD ? `https://www.youtube.com/watch?v=${videos[0].id.videoId}` : '';
     } else {
-      console.log(`Error: ${response.statusText}`);
+      logger.log(`Error: ${response.statusText}`);
     }
   } catch (error) {
-    console.log("Error fetching data:", error);
+    logger.log("Error fetching data:", error);
   }
 }
 
@@ -200,7 +202,7 @@ setInterval(async () => {
   }
 
   if (cachedTracks.length <= 0) {
-    console.log('---\n', `[${getTimeStamp()}] No new tracks to post! Trying again at ${getTimeStamp(postInterval).toLocaleString()}`)
+    logger.log(`[${getTimeStamp()}] No new tracks to post! Trying again at ${getTimeStamp(postInterval).toLocaleString()}`)
     return;
   }
 
@@ -214,13 +216,13 @@ setInterval(async () => {
       // ... 'Music');          // Check subreddit rules for each, fairly strict
       // ... 'listentothis');
 
-      console.log('---\n', `[${getTimeStamp()}]`, 'Posted', cachedTrack.postTitle, `from ${url}`);
+      logger.log(`[${getTimeStamp()}] Posted ${cachedTrack.postTitle} from ${url}`);
 
       postedTracks.push(cachedTrack.id);
       savePostedTracks();
     }
   } else {
-    console.log('Track', cachedTrack.postTitle, 'already posted; removing from playlist.')
+    logger.log(`Track ${cachedTrack.postTitle} already posted; removing from playlist.`)
   }
 
   removeTrackFromPlaylist(process.env.S_PLAYLIST, cachedTrack.id);
@@ -228,4 +230,4 @@ setInterval(async () => {
 }, postInterval);
 // }, postInterval + Math.floor(Math.random() * 3600000));
 
-console.log(`Script started at ${getTimeStamp()}; first post will occur at ${getTimeStamp(postInterval).toLocaleString()} plus a random amount < 1 hr.`);
+logger.log(`Script started at ${getTimeStamp()}; first post will occur at ${getTimeStamp(postInterval).toLocaleString()} plus a random amount < 1 hr.`);
---
+++ modules/logger.js
@@ -0,0 +1,39 @@
+const fs = require('fs');
+
+const maxSessionLogs = 3;
+const logDir = './logs';
+
+class Logger {
+    constructor(log, sessionLog) {
+        this.logFilePath = log;
+        this.sessionLogFilePath = sessionLog;
+
+        this.createLogFile(this.logFilePath);
+        this.createLogFile(this.sessionLogFilePath );
+
+        this.limitSessionLogs();
+    }
+
+    limitSessionLogs() {
+        fs.readdir(logDir, (err, files) => {
+            if(err) { 
+                console.log('Error reading log directory:', err)
+                return;
+            }
+            for(let i = 0; i < files.length - maxSessionLogs-1; i++){
+                fs.unlinkSync(`./logs/${files[i]}`);
+            }
+        });
+    }
+
+    createLogFile(path) {
+        if (!fs.existsSync(path)) fs.writeFileSync(path, '');
+    }
+
+    log(msg) {
+        fs.appendFileSync(this.logFilePath, msg + '\n');
+        fs.appendFileSync(this.sessionLogFilePath, msg + '\n');
+    }
+}
+
+module.exports = Logger;
